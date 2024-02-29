ff340968d45c371611985e2df8fac0be2dc88f2e
raphael@offshorly.com <raphael@offshorly.com>
2024-02-29 15:43:48 +0800

    updated build script

--- build.sh
+++ build.sh
@@ -8,12 +8,14 @@ APPIMAGE_URL="https://appimages.libreitalia.org/LibreOffice-fresh.full-x86_64.Ap
 
 # Download LibreOffice AppImage
 echo "Downloading LibreOffice AppImage..."
-wget -q -O "${HOME}/LibreOffice.AppImage" "$APPIMAGE_URL"
+wget -q -O "/opt/render/LibreOffice.AppImage" "$APPIMAGE_URL"
 
 # Make the AppImage executable
-chmod +x "${HOME}/LibreOffice.AppImage"
+chmod +x /opt/render/LibreOffice*.AppImage
+ls -la /opt/render/LibreOffice*.AppImage
 
 # Optionally, add a function to run LibreOffice to your .bashrc or .profile for convenience
-echo "alias libreoffice='${HOME}/LibreOffice.AppImage'" >> ${HOME}/.bashrc
+echo 'alias libreoffice="/opt/render/LibreOffice*.AppImage --appimage-extract-and-run"' >> ~/.bash_profile
+source ~/.bash_profile
 
 echo "LibreOffice AppImage download complete. You can run it with ./LibreOffice.AppImage or use the alias 'libreoffice' if you restart your terminal or source your profile/bashrc file."
