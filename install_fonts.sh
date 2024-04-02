76e7dc9657463d1e4ca6c9e84e433d97eeced02b
raphael@offshorly.com <raphael@offshorly.com>
2024-04-02 20:08:20 +0800

    fixed script

--- install_fonts.sh
+++ install_fonts.sh
@@ -7,7 +7,7 @@ echo "Creating font directory if it doesn't exist"
 mkdir -p $FONT_DIR
 
 echo "Copying fonts to font directory"
-cp helpers/.fonts*.ttf $FONT_DIR/
+cp helpers/.fonts/*.ttf $FONT_DIR/
 
 echo "Updating font cache"
 fc-cache -fv
