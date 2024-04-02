c74697c2b610a294ac22b99903357fdf42a989c8
raphael@offshorly.com <raphael@offshorly.com>
2024-04-02 19:33:15 +0800

    modified font name

--- helpers/convert2pdf_spire.py
+++ helpers/convert2pdf_spire.py
@@ -24,7 +24,7 @@ def convert_to_pdf(input_file_path):
     fonts.append(PrivateFontPath("NotoSansThai",FontStyle.Regular,os.path.join(output_directory,".fonts/NotoSansThai-VariableFont_wght.ttf")))
     fonts.append(PrivateFontPath("Arial",FontStyle.Regular,os.path.join(output_directory,".fonts/arial.ttf")))
     fonts.append(PrivateFontPath("MS Gothic",FontStyle.Regular,os.path.join(output_directory,".fonts/MS Gothic.ttf")))
-    fonts.append(PrivateFontPath("Microsoft Yahei UI",FontStyle.Regular,os.path.join(output_directory,".fonts/MicrosoftYaHeiUI.ttf")))
+    fonts.append(PrivateFontPath("Microsoft YaHei UI",FontStyle.Regular,os.path.join(output_directory,".fonts/MicrosoftYaHeiUI.ttf")))
     fonts.append(PrivateFontPath("SimSun",FontStyle.Regular,os.path.join(output_directory,".fonts/SimSun.ttf")))
     fonts.append(PrivateFontPath("Times New Roman",FontStyle.Regular,os.path.join(output_directory,".fonts/Times New Roman.ttf")))
 
