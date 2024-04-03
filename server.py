7a65a557d4245bf0a535fd64652347bd5e64d3d2
raphael@offshorly.com <raphael@offshorly.com>
2024-04-03 14:43:31 +0800

    revert back cors origins setting

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ import json
 app = Flask(__name__)
 # socketio = SocketIO(app, cors_allowed_origins="*")
 # task_queue = Queue()
-CORS(app, resources={r"/api/*": {"origins": "https://specialized.offshorly.com"}})
+CORS(app)
 
 cache = SimpleCache()
 # work_available = Event()
