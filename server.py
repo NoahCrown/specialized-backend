d959600852af101d57bd5c256257dc4e82dea3ed
raphael@offshorly.com <raphael@offshorly.com>
2024-08-20 17:06:10 +0800

    fix on port issue

--- server.py
+++ server.py
@@ -834,12 +834,11 @@ def bulk_custom_prompt():
 #     # Clean up
 #     p.join()
 
+@app.route('/', method=['POST'])
 def initialize_vector_store():
     vector_store = JobDescriptionVectorStore()
     if vector_store.is_empty():
         csv_file_path = './data/Moribian_Data.csv'
         load_job_descriptions_from_csv(csv_file_path, vector_store)
     return vector_store
-
-vector_store = initialize_vector_store()
 
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
+from waitress import serve
 from server import app
+import multiprocessing
 
 if __name__ == "__main__":
-    app.run()
+    serve(app, host='0.0.0.0', port=10000, 
+          threads=multiprocessing.cpu_count() * 2)
